b9d3e4f98ae58c65bcf3140dac8173364c16fc3b
Antikcore0 <fxancudle17@gmail.com>
2025-01-02 17:26:08 +0200

    Added adaptation and small fixes

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 new Splide('#slider1', {
     type: 'loop',
     perPage: 5,
-    gap: '1.1em'
+    pagination: false,
+    gap: '1.1em',
+    breakpoints: {
+        1200: {
+            perPage: 3,
+        },
+        900: {
+            perPage: 2,
+        },
+        600: {
+            perPage: 1,
+        },
+    }
 }).mount();
+
 var splide = new Splide('#slider2', {
     arrows: false,
     perPage: 1,
@@ -12,7 +25,6 @@ const track = document.querySelector('#slider2 .splide__track');
 const list = track.querySelector('#slider2 .splide__list');
 
 splide.on('moved', (index) => {
-    console.log(`Moved to slide: ${index}`);
     const translateX = `${index * -100}%`;
     list.style.transform = `translateX(${translateX})`;
 });
